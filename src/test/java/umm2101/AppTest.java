6e03e8beafcca8e6b3d388d8d9065d10a78f6ac7
K.K. Lamberty <lamberty@morris.umn.edu>
2021-03-30 22:22:22 -0500

    Add Java files, pom.xml, and some elements of .gitignore
    This project will now include the very basic things needed to get started with a Java project. The current Java version target is 14, and it will probably make sense to change to the most recent LTS release of Java before the class begins.
    
    My goal here was to get the minimum needed to run something as a Java project in VS Code so I could begin exploring using that environment for CSci 2101.

---
+++ src/test/java/umm2101/AppTest.java
@@ -0,0 +1,19 @@
+package umm2101;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * A very basic test just to see what tests might look like.
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
